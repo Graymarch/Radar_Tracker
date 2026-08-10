5252c07b4b0d3cc26b642c4d1fd5984d843272f1
Mitch <mgriff30@towson.edu>
2026-08-10 14:49:50 -0400

    Added skeleton for the Simulator.java class.

---
+++ Radar-Tracker/src/Simulator.java
@@ -0,0 +1,13 @@
+import trackers.*;
+import java.util.ArrayList;
+
+public class Simulator{
+	
+	private static final double tick = 0.5;
+	private static final int max_trackers = 10;
+	
+//	private final ArrayList<Tracker> activeTrackers;
+//	private final TrackerFactory factory;
+	
+	
+}
