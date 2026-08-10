5527cd0dd216a6d6bf863492289b73ebc9e6d4f7
Mitch <mgriff30@towson.edu>
2026-08-10 18:31:46 -0400

    Fleshed out main.java

--- Radar-Tracker/src/Main.java
+++ Radar-Tracker/src/Main.java
@@ -1,4 +1,24 @@
+import java.util.concurrent.TimeUnit;
+
+import observer_subject.*;
+import trackers.*;
+
 public class Main{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		TrackerBroadcaster broadcaster = new TrackerBroadcaster();
+		broadcaster.subscribe(new ConsoleLogger());
+		broadcaster.subscribe(new DefaultAnalyzer());
+		
+		TrackerFactory factory = new RandomTrackerFactory();
+		
+		Simulator simulator = new Simulator(factory, broadcaster);
+		
+		System.out.println("Starting radar track simulation (30s demo)...\n");
+        simulator.start();
+
+        TimeUnit.SECONDS.sleep(30);
+
+        simulator.stop();
+        System.out.println("\nSimulation stopped.");
 	}
 }
